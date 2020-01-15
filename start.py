a802861f905811c5ce74cbd51f26bff66808332a
Oleh <lemeholeh@gmail.com>
2020-01-15 23:15:50 +0200

    python Dz

---
+++ start.py
@@ -0,0 +1,146 @@
+                                                                          ############### DZ ################      
+
+
+
+
+# ==================================================== #
+# пора дня від введеного часу
+
+#a = int(input("Введіть час: "))
+# if a <= 6:
+#  print("Ніч")
+# elif a <= 12:
+# print("РАнок")
+# elif a <= 18:
+# print ("День")
+# elif a <= 24:
+# print("Ввечір")
+# else :
+#  print("errror")
+
+
+
+
+# ==================================================== #
+                ###КОНВЕНТР###
+
+
+#dlina = int(input(' 1. Кілометри -> Милі\n 2. Метри -> Ярди\n 3. Метри -> Фути\n 4. Сантиметри -> Дюйми\
+        \n 5. Миліметри -> Дюйми \n 6. Милі -> Кілометри \n 7. Ярди -> Метри \n 8. Фути -> Метри\
+        \n 9. Дюйми -> Сантиметри\n 10. Дюйми -> Міліметри\n '))
+#if dlina == 1:
+    value_dlina = float(input('Скільки кілометрів? :\n'))
+    print(value_dlina, 'кілометрів становить',
+          round((value_dlina * 1.60934), 2), 'миль')
+#elif dlina == 2:
+    value_dlina = float(input('Скільки метрів? :\n'))
+    print(value_dlina, 'метров становить', round(
+        (value_dlina * 1.09361), 2), 'ярдів')
+#elif dlina == 3:
+    value_dlina = float(input('Скільки метрів? :\n'))
+    print(value_dlina, 'метров становить', round(
+        (value_dlina / 0.3048), 2), 'футів')
+#elif dlina == 4:
+    value_dlina = float(input('Скільки сантиметрів? :\n'))
+    print(value_dlina, 'сантиметрів становить', round(
+        (value_dlina * 2.5400013716), 2), 'дюймів')
+#elif dlina == 5:
+    value_dlina = float(input('Скільки міліметрів? :\n'))
+    print(value_dlina, 'міліметрів становить', round(
+        (value_dlina / 0.0393701), 2), 'дюймів')
+#elif dlina == 6:
+        value_dlina = float(input('Скільки миль? :\n'))
+        print(value_dlina, 'миль =', round(
+            (value_dlina / 0.621371), 2), 'кілометрів')
+#elif dlina == 7:
+        value_dlina = float(input('Скільки ярдів? :\n'))
+        print(value_dlina, 'ярдів =', round(
+            (value_dlina / 0.9144), 2), 'метрів')
+#elif dlina == 8:
+        value_dlina = float(input('Скількі футів? :\n'))
+        print(value_dlina, 'футів =', round(
+            (value_dlina / 3.28084), 2), 'метрів')
+#elif dlina == 9:
+        value_dlina = float(input('Скількі дюймів? :\n'))
+        print(value_dlina, 'дюймів =', round((value_dlina / 0.393701), 2), 'сантиметрів')
+#elif dlina == 10:
+        value_dlina = float(input('Скільки міліметрів? :\n'))
+        print(value_dlina,'міліметрів становить', round((value_dlina / 0.0393701), 2) ,'дюймів')
+
+
+                                                                    ############### END DZ ################      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#a = 10
+#b = 10
+# if a > b:
+#   print("a > b")
+# elif a < b:
+#   print("a < b")
+# else:
+#   print("a = b")
+
+#name =int (input("WWWWW00"))
+#print("hi", name)
+#print("type", type(name))
+
+
+#a = int(input("type: "))
+#b = int(input("type2: "))
+#func = input("func: + - * /")
+# if func == "+":
+#   print(a + b)
+# elif func == "/":
+#   if b == 0:
+#      print("Div 0")
+# else:
+#    print(a / b)
+# elif func == "-":
+#   print(a - b)
+# elif func == "*":
+#   print(a * b)
+
+# Показуэ пору року
+
+# a = int(input("tupe numb: "))
+# if a == 1 or a == 2 or a == 12:
+# print("winter")
+# elif a == 3 or a == 4 or a == 5:
+# print("весна")
+# elif a == 6 or a == 7 or a == 8:
+#  print("ЛІТО")
+# elif a == 9 or a == 10 or a == 11:
+#  print("Осынь")
+# else :
+#  print("errror")
+
+
+                                                                            ############### DZ ################      
